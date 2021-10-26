f1c0a90e2048f0eb3e5421851611ae1fbc5e781f
Daniel Morris <drdanmorris@gmail.com>
2021-10-26 16:56:03 +1300

    Update CursorFileComparer.cs

--- dotNET/FileComparison/CursorFileComparer.cs
+++ dotNET/FileComparison/CursorFileComparer.cs
@@ -1,22 +1,12 @@
-﻿using PulseCommon.Tests.UnitTests.New.Helpers.Extensions;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PulseCommon.Tests.UnitTests.New.Helpers.FileComparer
+namespace FileComparer
 {
     public class CursorFileComparer
     {
-
         public static FileComparison Compare(string fileA, string fileB, string filenameA = "FileA", string filenameB = "FileB")
         {
             var indexA = FileIndex.Create(fileA, filenameA);
             var indexB = FileIndex.Create(fileB, filenameB);
             return indexA.CompareWith(indexB);
         }
-
     }
 }
