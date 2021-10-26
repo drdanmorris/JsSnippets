a7bca3fecc727d109eca455deab10322c50cf00f
Daniel Morris <drdanmorris@gmail.com>
2021-10-26 17:00:09 +1300

    Update LineContext.cs

--- dotNET/FileComparison/LineContext.cs
+++ dotNET/FileComparison/LineContext.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
-namespace PulseCommon.Tests.UnitTests.New.Helpers.FileComparer
+namespace FileComparer
 {
     public class LineContext
     {
