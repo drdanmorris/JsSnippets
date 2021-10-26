b4cf3018e3318acd6c86274f6f1228ccfe294adc
Daniel Morris <drdanmorris@gmail.com>
2021-10-26 17:00:47 +1300

    Update LineType.cs

--- dotNET/FileComparison/LineType.cs
+++ dotNET/FileComparison/LineType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PulseCommon.Tests.UnitTests.New.Helpers.FileComparer
+namespace FileComparer
 {
     public enum LineType
     {
