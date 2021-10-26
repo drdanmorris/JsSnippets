428afc9797af1b59c6e605f2a877850305b3a790
Daniel Morris <drdanmorris@gmail.com>
2021-10-26 16:58:11 +1300

    Update FileDiffs.cs

--- dotNET/FileComparison/FileDiffs.cs
+++ dotNET/FileComparison/FileDiffs.cs
@@ -1,10 +1,6 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PulseCommon.Tests.UnitTests.New.Helpers.FileComparer
+namespace FileComparer
 {
     public class FileDiffs
     {
