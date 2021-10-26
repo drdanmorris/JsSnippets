86a51257f4fbac0f6a530ee3e8c4f66d8d052b6b
Daniel Morris <drdanmorris@gmail.com>
2021-10-26 16:56:45 +1300

    Update FieldContext.cs

--- dotNET/FileComparison/FieldContext.cs
+++ dotNET/FileComparison/FieldContext.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace PulseCommon.Tests.UnitTests.New.Helpers.FileComparer
+namespace FileComparer
 {
     [DebuggerDisplay("{Name}")]
     public class FieldContext
